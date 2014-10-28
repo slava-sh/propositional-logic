495be7633317d6c2b36c962b208fb181359790aa
Slava Shklyaev <shk.slava@gmail.com>
2014-10-28 20:59:50 +0200

    Use short names for common operations internally

--- src/Data/Logic/Propositional.hs
+++ src/Data/Logic/Propositional.hs
@@ -16,6 +16,15 @@ data Expr
     | Implication Expr Expr
     deriving Eq
 
+neg :: Expr -> Expr
+neg = Negation
+
+conj :: Expr -> Expr -> Expr
+conj = Conjunction
+
+disj :: Expr -> Expr -> Expr
+disj = Disjunction
+
 instance Show Expr where
     show formula = case formula of
             (Literal x)       -> [x]
@@ -33,40 +42,36 @@ toCNF :: Expr -> Expr
 toCNF = toCNF' . toNNF
 
 toCNF' :: Expr -> Expr
-toCNF' (Literal x)            = Literal x
-toCNF' (Negation (Literal x)) = Negation (Literal x)
-toCNF' (Conjunction x y)      = (Conjunction `on` toCNF') x y
-toCNF' (Disjunction x y)      = (distributeDisjunction `on` toCNF') x y
-toCNF' _                      = error "toCNF': incorrect expression"
+toCNF' x@(Literal _)            = x
+toCNF' x@(Negation (Literal _)) = x
+toCNF' (Conjunction x y)        = (conj `on` toCNF') x y
+toCNF' (Disjunction x y)        = (distributeDisj `on` toCNF') x y
+toCNF' _                        = error "toCNF': incorrect expression"
 
-distributeDisjunction :: Expr -> Expr -> Expr
-distributeDisjunction (Conjunction x1 x2) y =
-    Conjunction (distributeDisjunction x1 y) (distributeDisjunction x2 y)
-distributeDisjunction x (Conjunction y1 y2) =
-    Conjunction (distributeDisjunction x y1) (distributeDisjunction x y2)
-distributeDisjunction x y = Disjunction x y
+distributeDisj :: Expr -> Expr -> Expr
+distributeDisj (Conjunction x1 x2) y =
+    distributeDisj x1 y `conj` distributeDisj x2 y
+distributeDisj x (Conjunction y1 y2) =
+    distributeDisj x y1 `conj` distributeDisj x y2
+distributeDisj x y = x `disj` y
 
 toNNF :: Expr -> Expr
 toNNF = toNNF' . implicationFree
 
 toNNF' :: Expr -> Expr
-toNNF' (Literal x) = Literal x
-toNNF' (Conjunction x y) =
-    (Conjunction `on` toNNF') x y
-toNNF' (Disjunction x y) =
-    (Disjunction `on` toNNF') x y
+toNNF' x@(Literal _) = x
+toNNF' (Conjunction x y) = (conj `on` toNNF') x y
+toNNF' (Disjunction x y) = (disj `on` toNNF') x y
 toNNF' (Negation (Literal x)) = Negation (Literal x)
 toNNF' (Negation (Negation x)) = toNNF' x
-toNNF' (Negation (Conjunction x y)) =
-    (Disjunction `on` (toNNF' . Negation)) x y
-toNNF' (Negation (Disjunction x y)) =
-    (Conjunction `on` (toNNF' . Negation)) x y
+toNNF' (Negation (Conjunction x y)) = (disj `on` (toNNF' . neg)) x y
+toNNF' (Negation (Disjunction x y)) = (conj `on` (toNNF' . neg)) x y
 toNNF' _ = error "toNNF': incorrect expression"
 
 implicationFree :: Expr -> Expr
-implicationFree (Literal x)       = Literal x
-implicationFree (Negation x)      = Negation (implicationFree x)
-implicationFree (Conjunction x y) = (Conjunction `on` implicationFree) x y
-implicationFree (Disjunction x y) = (Disjunction `on` implicationFree) x y
+implicationFree x@(Literal _)     = x
+implicationFree (Negation x)      = neg (implicationFree x)
+implicationFree (Conjunction x y) = (conj `on` implicationFree) x y
+implicationFree (Disjunction x y) = (disj `on` implicationFree) x y
 implicationFree (Implication x y) =
-    Disjunction (Negation (implicationFree x)) (implicationFree y)
+    (neg (implicationFree x)) `disj` (implicationFree y)
