eab3249725df41ab9844f7997b6d71ec00aef348
Slava Shklyaev <shk.slava@gmail.com>
2014-10-29 00:37:10 +0200

    Indent with 2 spaces instead of 4

--- src/Data/Logic/Propositional.hs
+++ src/Data/Logic/Propositional.hs
@@ -1,45 +1,45 @@
 module Data.Logic.Propositional
-    ( Expr(..)
-    , Atom
-    , toCNF
-    , toNNF
-    ) where
+  ( Expr(..)
+  , Atom
+  , toCNF
+  , toNNF
+  ) where
 
 type Atom = Char
 
 data Expr
-    = Var Atom
-    | Neg Expr
-    | Expr :/\ Expr
-    | Expr :\/ Expr
-    | Expr :-> Expr
-    deriving Eq
+  = Var Atom
+  | Neg Expr
+  | Expr :/\ Expr
+  | Expr :\/ Expr
+  | Expr :-> Expr
+  deriving Eq
 
 infixl 3 :/\
 infixl 2 :\/
 infixr 1 :->
 
 instance Show Expr where
-    showsPrec p expr = case expr of
-            (Var x)   -> showChar x
-            (Neg x)   -> showChar '¬' . showsPrec 4 x
-            (x :/\ y) -> showBinary " ∧ " 3 x y
-            (x :\/ y) -> showBinary " ∨ " 2 x y
-            (x :-> y) -> showBinary " → " 1 x y
-        where
-            showBinary op p' x y = showParen (p' < p) $
-                showsPrec p' x . showString op . showsPrec p' y
+  showsPrec p expr = case expr of
+      (Var x)   -> showChar x
+      (Neg x)   -> showChar '¬' . showsPrec 4 x
+      (x :/\ y) -> showBinary " ∧ " 3 x y
+      (x :\/ y) -> showBinary " ∨ " 2 x y
+      (x :-> y) -> showBinary " → " 1 x y
+    where
+      showBinary op p' x y =
+        showParen (p' < p) $ showsPrec p' x . showString op . showsPrec p' y
 
 toCNF :: Expr -> Expr
 toCNF = toCNF' . toNNF
-    where
-        toCNF' (x :/\ y) = toCNF' x :/\ toCNF' y
-        toCNF' (x :\/ y) = toCNF' x `dist` toCNF' y
-        toCNF' x         = x
+  where
+    toCNF' (x :/\ y) = toCNF' x :/\ toCNF' y
+    toCNF' (x :\/ y) = toCNF' x `dist` toCNF' y
+    toCNF' x         = x
 
-        dist (x1 :/\ x2) y = (x1 `dist` y) :/\ (x2 `dist` y)
-        dist x (y1 :/\ y2) = (x `dist` y1) :/\ (x `dist` y2)
-        dist x y           = x :\/ y
+    dist (x1 :/\ x2) y = (x1 `dist` y) :/\ (x2 `dist` y)
+    dist x (y1 :/\ y2) = (x `dist` y1) :/\ (x `dist` y2)
+    dist x y           = x :\/ y
 
 toNNF :: Expr -> Expr
 toNNF x@(Var _)       = x
--- tests/Test.hs
+++ tests/Test.hs
@@ -16,88 +16,88 @@ q = Var 'q'
 r = Var 'r'
 
 instance Arbitrary Expr where
-    arbitrary = sized expr
+  arbitrary = sized expr
+    where
+      expr 0 = elements [p, q, r]
+      expr n = oneof
+        [ (Neg) <$> expr n'
+        , (:/\) <$> expr n' <*> expr n'
+        , (:\/) <$> expr n' <*> expr n'
+        , (:->) <$> expr n' <*> expr n'
+        ]
         where
-            expr 0 = elements [p, q, r]
-            expr n = oneof
-                [ (Neg) <$> expr n'
-                , (:/\) <$> expr n' <*> expr n'
-                , (:\/) <$> expr n' <*> expr n'
-                , (:->) <$> expr n' <*> expr n'
-                ]
-                where
-                    n' = n `div` 2
+          n' = n `div` 2
 
 truthTable :: Expr -> [Bool]
 truthTable x = do
-    p <- [False, True]
-    q <- [False, True]
-    r <- [False, True]
-    return $ eval [('p', p), ('q', q), ('r', r)] x
+  p <- [False, True]
+  q <- [False, True]
+  r <- [False, True]
+  return $ eval [('p', p), ('q', q), ('r', r)] x
 
 eval :: [(Atom, Bool)] -> Expr -> Bool
 eval env = eval'
-    where
-        eval' (Var x)   = fromJust $ lookup x env
-        eval' (Neg x)   = not $ eval' x
-        eval' (x :/\ y) = eval' x && eval' y
-        eval' (x :\/ y) = eval' x || eval' y
-        eval' (x :-> y) = not (eval' x) || eval' y
+  where
+    eval' (Var x)   = fromJust $ lookup x env
+    eval' (Neg x)   = not $ eval' x
+    eval' (x :/\ y) = eval' x && eval' y
+    eval' (x :\/ y) = eval' x || eval' y
+    eval' (x :-> y) = not (eval' x) || eval' y
 
 isCNF :: Expr -> Bool
 isCNF (x :/\ y) = isCNF x && isCNF y
 isCNF x         = isDisj x
-    where
-        isDisj (x :\/ y) = isDisj x && isDisj y
-        isDisj x         = isLit x
+  where
+    isDisj (x :\/ y) = isDisj x && isDisj y
+    isDisj x         = isLit x
 
-        isLit (Var _)       = True
-        isLit (Neg (Var _)) = True
-        isLit _             = False
+    isLit (Var _)       = True
+    isLit (Neg (Var _)) = True
+    isLit _             = False
 
 containsNonatomicNegations :: Expr -> Bool
 containsNonatomicNegations = go
-    where
-        go (Var _)       = False
-        go (Neg (Var _)) = False
-        go (Neg _)       = True
-        go (x :/\ y)     = go x || go y
-        go (x :\/ y)     = go x || go y
-        go (x :-> y)     = go x || go y
+  where
+    go (Var _)       = False
+    go (Neg (Var _)) = False
+    go (Neg _)       = True
+    go (x :/\ y)     = go x || go y
+    go (x :\/ y)     = go x || go y
+    go (x :-> y)     = go x || go y
 
 qcTests = testGroup "QuickCheck tests"
-    [ testProperty "CNF of x is semantically equivalent to x" $ \x ->
-          truthTable (toCNF x) == truthTable x
-    , testProperty "CNF is a conjunction of disjuncitons" $ \x ->
-          isCNF $ toCNF x
-    , testProperty "NNF of x is semantically equivalent to x" $ \x ->
-          truthTable (toNNF x) == truthTable x
-    , testProperty "In NNF only atoms are negated" $ \x ->
-          not . containsNonatomicNegations $ toNNF x
-    ]
+  [ testProperty "CNF of x is semantically equivalent to x" $ \x ->
+      truthTable (toCNF x) == truthTable x
+  , testProperty "CNF is a conjunction of disjuncitons" $ \x ->
+      isCNF $ toCNF x
+  , testProperty "NNF of x is semantically equivalent to x" $ \x ->
+      truthTable (toNNF x) == truthTable x
+  , testProperty "In NNF only atoms are negated" $ \x ->
+      not . containsNonatomicNegations $ toNNF x
+  ]
 
 cnfs =
-    [ p
-    , p :\/ Neg q
-    , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
-    , p :/\ q :/\ Neg r :/\ r
-    ]
+  [ p
+  , p :\/ Neg q
+  , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
+  , p :/\ q :/\ Neg r :/\ r
+  ]
 
 notCnfs =
-    [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
-    --           ^      ^
-    , (Neg p :\/ (r :/\ Neg q)) :\/ p
-    --              ^
-    , (Neg p :\/ (r :-> Neg q)) :\/ p
-    --              ^
-    , (p :\/ Neg (Neg q))
-    --       ^    ^
-    ]
+  [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
+  --           ^      ^
+  , (Neg p :\/ (r :/\ Neg q)) :\/ p
+  --              ^
+  , (Neg p :\/ (r :-> Neg q)) :\/ p
+  --              ^
+  , (p :\/ Neg (Neg q))
+  --       ^    ^
+  ]
 
 unitTests = testGroup "Unit tests"
-    [ testCase "CNF of an atom: p" $ toCNF p @?= p
-    , testCase "CNF of an atom: q" $ toCNF q @?= q
-    , testCase "CNF of an atom: r" $ toCNF r @?= r
-    , testCase "isCNF: True"  $ True  @=? all isCNF cnfs
-    , testCase "isCNF: False" $ False @=? any isCNF notCnfs
-    ]
+  [ testCase "CNF of an atom: p" $ toCNF p @?= p
+  , testCase "CNF of an atom: q" $ toCNF q @?= q
+  , testCase "CNF of an atom: r" $ toCNF r @?= r
+  , testCase "isCNF: True"  $ True  @=? all isCNF cnfs
+  , testCase "isCNF: False" $ False @=? any isCNF notCnfs
+  ]
