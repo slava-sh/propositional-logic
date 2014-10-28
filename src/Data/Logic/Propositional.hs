bf3ac3370c8a06c96cbe85712291afc7117f0637
Slava Shklyaev <shk.slava@gmail.com>
2014-10-28 21:27:39 +0200

    Refactor

--- src/Data/Logic/Propositional.hs
+++ src/Data/Logic/Propositional.hs
@@ -26,7 +26,7 @@ disj :: Expr -> Expr -> Expr
 disj = Disjunction
 
 instance Show Expr where
-    show formula = case formula of
+    show expr = case expr of
             (Literal x)       -> [x]
             (Negation x)      -> "¬" ++ parens x
             (Conjunction x y) -> showBinary "∧" x y
@@ -40,38 +40,22 @@ instance Show Expr where
 
 toCNF :: Expr -> Expr
 toCNF = toCNF' . toNNF
+    where
+        toCNF' (Conjunction x y) = (conj `on` toCNF') x y
+        toCNF' (Disjunction x y) = (dist `on` toCNF') x y
+        toCNF' x                 = x
 
-toCNF' :: Expr -> Expr
-toCNF' x@(Literal _)            = x
-toCNF' x@(Negation (Literal _)) = x
-toCNF' (Conjunction x y)        = (conj `on` toCNF') x y
-toCNF' (Disjunction x y)        = (distributeDisj `on` toCNF') x y
-toCNF' _                        = error "toCNF': incorrect expression"
-
-distributeDisj :: Expr -> Expr -> Expr
-distributeDisj (Conjunction x1 x2) y =
-    distributeDisj x1 y `conj` distributeDisj x2 y
-distributeDisj x (Conjunction y1 y2) =
-    distributeDisj x y1 `conj` distributeDisj x y2
-distributeDisj x y = x `disj` y
+        dist (Conjunction x1 x2) y = (x1 `dist` y) `conj` (x2 `dist` y)
+        dist x (Conjunction y1 y2) = (x `dist` y1) `conj` (x `dist` y2)
+        dist x y                   = x `disj` y
 
 toNNF :: Expr -> Expr
-toNNF = toNNF' . implicationFree
-
-toNNF' :: Expr -> Expr
-toNNF' x@(Literal _) = x
-toNNF' (Conjunction x y) = (conj `on` toNNF') x y
-toNNF' (Disjunction x y) = (disj `on` toNNF') x y
-toNNF' (Negation (Literal x)) = Negation (Literal x)
-toNNF' (Negation (Negation x)) = toNNF' x
-toNNF' (Negation (Conjunction x y)) = (disj `on` (toNNF' . neg)) x y
-toNNF' (Negation (Disjunction x y)) = (conj `on` (toNNF' . neg)) x y
-toNNF' _ = error "toNNF': incorrect expression"
-
-implicationFree :: Expr -> Expr
-implicationFree x@(Literal _)     = x
-implicationFree (Negation x)      = neg (implicationFree x)
-implicationFree (Conjunction x y) = (conj `on` implicationFree) x y
-implicationFree (Disjunction x y) = (disj `on` implicationFree) x y
-implicationFree (Implication x y) =
-    (neg (implicationFree x)) `disj` (implicationFree y)
+toNNF x@(Literal _) = x
+toNNF (Conjunction x y) = (conj `on` toNNF) x y
+toNNF (Disjunction x y) = (disj `on` toNNF) x y
+toNNF (Implication x y) = (disj `on` toNNF) (neg x) y
+toNNF (Negation (Literal x)) = Negation (Literal x)
+toNNF (Negation (Negation x)) = toNNF x
+toNNF (Negation (Conjunction x y)) = (disj `on` (toNNF . neg)) x y
+toNNF (Negation (Disjunction x y)) = (conj `on` (toNNF . neg)) x y
+toNNF (Negation (Implication x y)) = (conj `on` toNNF) x (neg y)
