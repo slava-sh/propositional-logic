cadc811b0a14a8dff4ea3cbae5e645a77242caca
Slava Shklyaev <shk.slava@gmail.com>
2014-10-28 13:15:50 +0200

    Declare API

--- Main.hs
+++ Main.hs
@@ -1,3 +1,26 @@
+data Formula
+    = Literal String
+    | Not     Formula
+    | And     Formula Formula
+    | Or      Formula Formula
+    | Implies Formula Formula
+    deriving Show
+
+conjunctiveNormalForm :: Formula -> Formula
+conjunctiveNormalForm = conjunctiveNormalForm' . negationNormalForm . implicationFree
+
+conjunctiveNormalForm' :: Formula -> Formula
+conjunctiveNormalForm' = id
+
+negationNormalForm :: Formula -> Formula
+negationNormalForm = id
+
+implicationFree :: Formula -> Formula
+implicationFree = id
+
 main :: IO ()
 main = do
-	putStrLn "Transform a propositional logic formula into an equivalent formula in CNF"
+    putStrLn "Transform a propositional logic formula into an equivalent formula in CNF"
+    let x = Not (Not (Literal "a" `Or` Not (Literal "b"))) `And` (Literal "a" `Implies` Literal "b")
+    print x
+    print $ conjunctiveNormalForm x
