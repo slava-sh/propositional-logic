b5a99f62fcd01359f6276ea6268a84e088ea1f6f
Slava Shklyaev <shk.slava@gmail.com>
2014-10-29 15:32:47 +0200

    Refactor tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -10,7 +10,74 @@ import Data.Logic.Propositional
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [qcTests, unitTests]
+tests = testGroup "Data.Logic.Propositional"
+  [ testCase "CNF of an atom: p" $ toCNF p @?= p
+  , testCase "CNF of an atom: q" $ toCNF q @?= q
+  , testCase "CNF of an atom: r" $ toCNF r @?= r
+  , testCase "DNF of an atom: p" $ toDNF p @?= p
+  , testCase "DNF of an atom: q" $ toDNF q @?= q
+  , testCase "DNF of an atom: r" $ toDNF r @?= r
+  , testCase "isCNF: True"  $ True  @=? all isCNF cnfs
+  , testCase "isCNF: False" $ False @=? any isCNF notCnfs
+  , testCase "isDNF: True"  $ True  @=? all isDNF dnfs
+  , testCase "isDNF: False" $ False @=? any isDNF notDnfs
+  , testCase "show Expr" $
+      show (p :\/ q :\/ r :/\ r :\/ ((r :\/ q) :/\ q) :\/ q)
+        @?= "(p ∨ (q ∨ ((r ∧ r) ∨ (((r ∨ q) ∧ q) ∨ q))))"
+  , testCase "show Expr" $
+      show (p :/\ q :-> p :\/ q :-> Neg r)
+        @?= "((p ∧ q) → ((p ∨ q) → ¬r))"
+  , testProperty "CNF of x is semantically equivalent to x" $ \x ->
+      toCNF x `equals` x
+  , testProperty "DNF of x is semantically equivalent to x" $ \x ->
+      toDNF x `equals` x
+  , testProperty "NNF of x is semantically equivalent to x" $ \x ->
+      toNNF x `equals` x
+  , testProperty "CNF is a conjunction of disjuncitons of literals" $ \x ->
+      isCNF $ toCNF x
+  , testProperty "DNF is a disjunction of conjuncitons of literals" $ \x ->
+      isDNF $ toDNF x
+  , testProperty "In NNF only atoms are negated" $ \x ->
+      not . containsNonatomicNegations $ toNNF x
+  ]
+
+cnfs =
+  [ p
+  , p :\/ Neg q
+  , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
+  , p :/\ q :/\ Neg r :/\ r
+  , p :/\ q :/\ (Neg r :\/ ((q :\/ r) :\/ p)) :/\ r
+  ]
+
+notCnfs =
+  [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
+  --           ^      ^
+  , (Neg p :\/ (r :/\ Neg q)) :\/ p
+  --              ^
+  , (Neg p :\/ (r :-> Neg q)) :\/ p
+  --              ^
+  , p :\/ Neg (Neg q)
+  --      ^    ^
+  ]
+
+dnfs =
+  [ p
+  , p :\/ Neg q
+  , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
+  , p :/\ q :/\ Neg r :/\ r
+  , p :\/ q :\/ (Neg r :/\ ((q :/\ r) :/\ p)) :\/ r
+  ]
+
+notDnfs =
+  [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
+  --           ^      ^
+  , (Neg p :/\ (r :\/ Neg q)) :/\ p
+  --              ^
+  , (Neg p :\/ (r :-> Neg q)) :\/ p
+  --              ^
+  , p :\/ Neg (Neg q)
+  --      ^    ^
+  ]
 
 p = Var 'p'
 q = Var 'q'
@@ -73,75 +140,3 @@ containsNonatomicNegations = go
     go (x :/\ y)     = go x || go y
     go (x :\/ y)     = go x || go y
     go (x :-> y)     = go x || go y
-
-qcTests = testGroup "QuickCheck tests"
-  [ testProperty "CNF of x is semantically equivalent to x" $ \x ->
-      toCNF x `equals` x
-  , testProperty "DNF of x is semantically equivalent to x" $ \x ->
-      toDNF x `equals` x
-  , testProperty "NNF of x is semantically equivalent to x" $ \x ->
-      toNNF x `equals` x
-  , testProperty "CNF is a conjunction of disjuncitons of literals" $ \x ->
-      isCNF $ toCNF x
-  , testProperty "DNF is a disjunction of conjuncitons of literals" $ \x ->
-      isDNF $ toDNF x
-  , testProperty "In NNF only atoms are negated" $ \x ->
-      not . containsNonatomicNegations $ toNNF x
-  ]
-
-cnfs =
-  [ p
-  , p :\/ Neg q
-  , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
-  , p :/\ q :/\ Neg r :/\ r
-  , p :/\ q :/\ (Neg r :\/ ((q :\/ r) :\/ p)) :/\ r
-  ]
-
-notCnfs =
-  [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
-  --           ^      ^
-  , (Neg p :\/ (r :/\ Neg q)) :\/ p
-  --              ^
-  , (Neg p :\/ (r :-> Neg q)) :\/ p
-  --              ^
-  , p :\/ Neg (Neg q)
-  --      ^    ^
-  ]
-
-dnfs =
-  [ p
-  , p :\/ Neg q
-  , (Neg p :\/ (r :\/ Neg q)) :\/ (Neg q :\/ (Neg p :\/ q))
-  , p :/\ q :/\ Neg r :/\ r
-  , p :\/ q :\/ (Neg r :/\ ((q :/\ r) :/\ p)) :\/ r
-  ]
-
-notDnfs =
-  [ (Neg p :\/ Neg (r :\/ Neg q)) :\/ p
-  --           ^      ^
-  , (Neg p :/\ (r :\/ Neg q)) :/\ p
-  --              ^
-  , (Neg p :\/ (r :-> Neg q)) :\/ p
-  --              ^
-  , p :\/ Neg (Neg q)
-  --      ^    ^
-  ]
-
-unitTests = testGroup "Unit tests"
-  [ testCase "CNF of an atom: p" $ toCNF p @?= p
-  , testCase "CNF of an atom: q" $ toCNF q @?= q
-  , testCase "CNF of an atom: r" $ toCNF r @?= r
-  , testCase "DNF of an atom: p" $ toDNF p @?= p
-  , testCase "DNF of an atom: q" $ toDNF q @?= q
-  , testCase "DNF of an atom: r" $ toDNF r @?= r
-  , testCase "isCNF: True"  $ True  @=? all isCNF cnfs
-  , testCase "isCNF: False" $ False @=? any isCNF notCnfs
-  , testCase "isDNF: True"  $ True  @=? all isDNF dnfs
-  , testCase "isDNF: False" $ False @=? any isDNF notDnfs
-  , testCase "show Expr" $
-      show (p :\/ q :\/ r :/\ r :\/ ((r :\/ q) :/\ q) :\/ q)
-        @?= "(p ∨ (q ∨ ((r ∧ r) ∨ (((r ∨ q) ∧ q) ∨ q))))"
-  , testCase "show Expr" $
-      show (p :/\ q :-> p :\/ q :-> Neg r)
-        @?= "((p ∧ q) → ((p ∨ q) → ¬r))"
-  ]
