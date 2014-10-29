444bc7798033f5764af8fed18f8c232d1e154ddb
Slava Shklyaev <shk.slava@gmail.com>
2014-10-29 22:17:41 +0200

    Implement parseExpr

--- src/Data/Logic/Propositional/Parser.hs
+++ src/Data/Logic/Propositional/Parser.hs
@@ -3,19 +3,39 @@ module Data.Logic.Propositional.Parser
   ) where
 
 import Text.Parsec
+import Text.Parsec.String
 import Text.Parsec.Expr
-import Control.Applicative
+import Data.Functor.Identity
+import Control.Applicative hiding ((<|>))
 
 import Data.Logic.Propositional
 
 parseExpr :: SourceName -> String -> Either ParseError Expr
 parseExpr = runP expr ()
 
-expr :: Parsec String () Expr
-expr = buildExpressionParser operators term
+expr :: Parser Expr
+expr = spaces *> buildExpressionParser table term
+  <?> "expression"
 
-term = do
-  x <- letter
-  return $ Var x
+term :: Parser Expr
+term = parens expr <|> var
+  <?> "term"
 
-operators = []
+var :: Parser Expr
+var = Var <$> (letter <* spaces)
+  <?> "variable"
+
+table :: OperatorTable String u Identity Expr
+table =
+  [ [unary  "~"  Neg]
+  , [binary "&"  (:/\)]
+  , [binary "|"  (:\/)]
+  , [binary "->" (:->)]
+  ]
+  where
+    unary   op f = Prefix $ parseOp op f `chainl1` return (.)
+    binary  op f = Infix (parseOp op f) AssocRight
+    parseOp op f = string op *> spaces *> return f
+
+parens :: Parser a -> Parser a
+parens = between (char '(' *> spaces) (char ')' *> spaces)
